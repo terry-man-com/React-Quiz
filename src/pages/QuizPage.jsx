d5ea445587f52472b1b64c5ee3b717594cd9df9c
terry-man-com <terry.man.com@icloud.com>
2026-07-14 08:25:50 +0900

    edit: QuizPage.jsx

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -1,12 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import Button from "../components/Button/Button";
 import Display from "../components/Display/Display"
 import quizData from "../data/quiz"
+import { useState } from 'react';
 
 export default function QuizPage() {
-    const quizIndex = 0;
+    const [quizIndex, setQuizIndex] = useState(0);
+    const [answerLogs, setAnswerLogs] = useState([]);
+    const navigation = useNavigate();
+    const MAX_QUIZ_LEN = quizData.length
+
     const handleClick = (clickedIndex) => {
-        console.log("clickedIndex:", clickedIndex);
-    }
+        if(clickedIndex === quizData[quizIndex].answerIndex) {
+            setAnswerLogs((prev) => [...prev, true])
+        } else {
+            setAnswerLogs((prev) => [...prev, false])
+        }
+        setQuizIndex((prev) => prev + 1);
+    };
   return (
     <>
         <Display>
