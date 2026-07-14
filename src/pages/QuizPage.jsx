80187deb221b84da4a598b9c1c4416b376d48cb8
terry-man-com <terry.man.com@icloud.com>
2026-07-15 08:25:51 +0900

    edit: QuizPage.jsx

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -2,7 +2,8 @@ import { useNavigate } from "react-router-dom";
 import Button from "../components/Button/Button";
 import Display from "../components/Display/Display"
 import quizData from "../data/quiz"
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { ROUTES } from "../const";
 
 export default function QuizPage() {
     const [quizIndex, setQuizIndex] = useState(0);
@@ -18,13 +19,24 @@ export default function QuizPage() {
         }
         setQuizIndex((prev) => prev + 1);
     };
+
+    useEffect(() => {
+        if(answerLogs.length === MAX_QUIZ_LEN){
+            const correctNum = answerLogs.filter((answer) => {
+                return answer === true
+            })
+            navigation(ROUTES.RESULT, {
+                state: {
+                    maxQuizLen: MAX_QUIZ_LEN,
+                    correctNum: correctNum
+                }
+            });
+        }
+    },[answerLogs, MAX_QUIZ_LEN, navigation]);
   return (
     <>
-        <Display>
-            {`Q1. ${quizData[quizIndex].question}`}
-        </Display>
-        {
-            quizData[quizIndex].options.map((option, index) => {
+        {quizData[quizIndex] && <Display>{`Q1. ${quizData[quizIndex].question}`}</Display>}
+        {quizData[quizIndex] && quizData[quizIndex].options.map((option, index) => {
                 return (
                     <Button key={`option-${index}`} onClick={() => handleClick(index)}>
                         {option}
