73617ab6c9b9ad58a4ad39400f29f703cd6177cb
terry-man-com <terry.man.com@icloud.com>
2026-07-16 07:48:50 +0900

    edit Result.jsx

--- src/pages/QuizPage.jsx
+++ src/pages/QuizPage.jsx
@@ -13,29 +13,29 @@ export default function QuizPage() {
 
     const handleClick = (clickedIndex) => {
         if(clickedIndex === quizData[quizIndex].answerIndex) {
-            setAnswerLogs((prev) => [...prev, true])
+            setAnswerLogs(prev => [...prev, true])
         } else {
-            setAnswerLogs((prev) => [...prev, false])
+            setAnswerLogs(prev => [...prev, false])
         }
-        setQuizIndex((prev) => prev + 1);
+        setQuizIndex(prev => prev + 1);
     };
 
     useEffect(() => {
         if(answerLogs.length === MAX_QUIZ_LEN){
-            const correctNum = answerLogs.filter((answer) => {
-                return answer === true
-            })
+            const correctNum = answerLogs.filter(answer => 
+                answer === true
+            )
             navigation(ROUTES.RESULT, {
                 state: {
                     maxQuizLen: MAX_QUIZ_LEN,
-                    correctNum: correctNum
+                    correctNumLen: correctNum.length
                 }
             });
         }
     },[answerLogs, MAX_QUIZ_LEN, navigation]);
   return (
     <>
-        {quizData[quizIndex] && <Display>{`Q1. ${quizData[quizIndex].question}`}</Display>}
+        {quizData[quizIndex] && <Display>{`Q${quizIndex + 1}. ${quizData[quizIndex].question}`}</Display>}
         {quizData[quizIndex] && quizData[quizIndex].options.map((option, index) => {
                 return (
                     <Button key={`option-${index}`} onClick={() => handleClick(index)}>
--- src/pages/ResultPage.jsx
+++ src/pages/ResultPage.jsx
@@ -1,5 +1,17 @@
+import { Link, useLocation } from "react-router-dom"
+import { ROUTES } from '../const';
+import Result from "../components/Result/Result";
+
 export default function ResultPage() {
+    const location = useLocation();
+    const maxQuizLen = location.state.maxQuizLen
+    const correctNumLen = location.state.correctNumLen
   return (
-    <div>ResultPage</div>
+    <>
+        <h1>Result</h1>
+        <Result maxQuizLen={maxQuizLen} correctNumLen={correctNumLen} />
+        <br />
+        <Link to={ROUTES.QUIZ}>もう一度チャレンジ</Link>
+    </>
   )
 }
